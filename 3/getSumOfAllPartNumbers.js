4410a16ff68e3b71708ac2589bf70e1fb909ad8b
Bryan <morganthemosaic@gmail.com>
2023-12-03 13:33:58 -0500

    Part 1

--- 3/getSumOfAllPartNumbers.js
+++ 3/getSumOfAllPartNumbers.js
@@ -11,95 +11,69 @@ for (const line of lines) {
 	engineSchematicArray.push(line.split(""));
 }
 
+const symbols = new Set();
+const symbolsRegex = /[^\d.]/;
+
+for (let i = 0; i < engineSchematicArray.length; i++) {
+	for (let j = 0; j < engineSchematicArray[i].length; j++) {
+		if (engineSchematicArray[i][j].match(symbolsRegex)) {
+			symbols.add(engineSchematicArray[i][j]);
+		}
+	}
+}
+
 let sumOfAllPartNumbers = 0;
-const symbols = new Set(["@", "#", "=", "*", "+", "$", "&", "%", "/", "-"]);
-const startIndicesOfPartNumbers = new Map();
+const allNumbers = [];
+const numberData = new Map();
+const validPartsNumbers = new Set();
 
 const isAdjacentToSymbol = (row, startingColumnIndex, numberOfDigits) => {
-	const columnIndexLeftOfNumber = startingColumnIndex - 1;
-	const columnIndexRightOfNumber = startingColumnIndex + numberOfDigits;
-	const rowIndexAboveNumber = row - 1;
-	const rowIndexBelowNumber = row + 1;
+	const left = startingColumnIndex - 1 >= 0 ? startingColumnIndex - 1 : 0;
+	const top = row - 1 >= 0 ? row - 1 : 0;
+	const right =
+		startingColumnIndex + numberOfDigits < engineSchematicArray[row].length
+			? startingColumnIndex + numberOfDigits
+			: engineSchematicArray[row].length - 1;
+	const bottom =
+		row + 1 < engineSchematicArray.length
+			? row + 1
+			: engineSchematicArray.length - 1;
 
-	if (
-		columnIndexLeftOfNumber >= 0 &&
-		symbols.has(engineSchematicArray[row][columnIndexLeftOfNumber])
-	) {
-		return true;
-	}
-
-	if (
-		columnIndexRightOfNumber < engineSchematicArray[0].length &&
-		symbols.has(engineSchematicArray[row][columnIndexRightOfNumber])
-	) {
-		return true;
-	}
-
-	if (rowIndexAboveNumber >= 0) {
-		for (
-			let i = startingColumnIndex;
-			i < startingColumnIndex + numberOfDigits;
-			i++
+	for (let i = left; i <= right; i++) {
+		if (
+			symbols.has(engineSchematicArray[top][i]) ||
+			symbols.has(engineSchematicArray[bottom][i])
 		) {
-			if (symbols.has(engineSchematicArray[rowIndexAboveNumber][i])) {
-				return true;
-			}
-		}
-	}
-
-	if (rowIndexBelowNumber < engineSchematicArray.length) {
-		for (
-			let i = startingColumnIndex;
-			i < startingColumnIndex + numberOfDigits;
-			i++
-		) {
-			if (symbols.has(engineSchematicArray[rowIndexBelowNumber][i])) {
-				return true;
-			}
+			return true;
 		}
 	}
 
 	if (
-		columnIndexLeftOfNumber >= 0 &&
-		rowIndexAboveNumber >= 0 &&
-		symbols.has(
-			engineSchematicArray[rowIndexAboveNumber][columnIndexLeftOfNumber]
-		)
-	) {
-		return true;
-	}
-
-	if (
-		columnIndexLeftOfNumber >= 0 &&
-		rowIndexBelowNumber < engineSchematicArray.length &&
-		symbols.has(
-			engineSchematicArray[rowIndexBelowNumber][columnIndexLeftOfNumber]
-		)
-	) {
-		return true;
-	}
-
-	if (
-		columnIndexRightOfNumber < engineSchematicArray[0].length &&
-		rowIndexAboveNumber >= 0 &&
-		symbols.has(
-			engineSchematicArray[rowIndexAboveNumber][columnIndexRightOfNumber]
-		)
-	) {
-		return true;
-	}
-
-	if (
-		columnIndexRightOfNumber < engineSchematicArray[0].length &&
-		rowIndexBelowNumber < engineSchematicArray.length &&
-		symbols.has(
-			engineSchematicArray[rowIndexBelowNumber][columnIndexRightOfNumber]
-		)
+		symbols.has(engineSchematicArray[row][left]) ||
+		symbols.has(engineSchematicArray[row][right])
 	) {
 		return true;
 	}
 
 	return false;
+};
+
+const processNumberString = (numberString, i, j) => {
+	const numberOfDigits = numberString.length;
+
+	if (numberOfDigits) {
+		const number = parseInt(numberString);
+		allNumbers.push(number);
+		const data = [i, j - numberOfDigits, numberOfDigits];
+
+		if (!numberData.has(number)) {
+			numberData.set(number, [data]);
+		} else {
+			const currentData = numberData.get(number);
+			currentData.push(data);
+			numberData.set(number, currentData);
+		}
+	}
 };
 
 for (let i = 0; i < engineSchematicArray.length; i++) {
@@ -108,32 +82,25 @@ for (let i = 0; i < engineSchematicArray.length; i++) {
 	for (let j = 0; j < engineSchematicArray[i].length; j++) {
 		const currentCharacter = engineSchematicArray[i][j];
 
-		if (!isNaN(parseInt(currentCharacter))) {
+		if (currentCharacter.match(/\d/)) {
 			numberString += currentCharacter;
 		} else {
-			const numberOfDigits = numberString.length;
-
-			if (numberOfDigits) {
-				startIndicesOfPartNumbers.set(parseInt(numberString), [
-					i,
-					j - numberOfDigits,
-				]);
-			}
+			processNumberString(numberString, i, j);
 
 			numberString = "";
 		}
 	}
+
+	processNumberString(numberString, i, engineSchematicArray[i].length);
+
+	numberString = "";
 }
 
-for (const [number, coordinates] of startIndicesOfPartNumbers) {
-	if (
-		isAdjacentToSymbol(
-			coordinates[0],
-			coordinates[1],
-			Number.toString(number).length
-		)
-	) {
-		sumOfAllPartNumbers += number;
+for (const [number, data] of numberData) {
+	for (const datum of data) {
+		if (isAdjacentToSymbol(datum[0], datum[1], datum[2])) {
+			sumOfAllPartNumbers += number;
+		}
 	}
 }
 

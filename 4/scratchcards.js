59319e1423adc4c6c4ed1588ef547a23566c7536
Bryan <morganthemosaic@gmail.com>
2023-12-04 08:05:01 -0500

    Part 2

--- 4/scratchcards.js
+++ 4/scratchcards.js
@@ -30,3 +30,39 @@ const partOne = (cards) => {
 
 	console.log(sumOfCardValues);
 };
+
+const partTwo = (cards) => {
+	const scratchcards = new Map();
+
+	for (let cardNumber = 0; cardNumber < cards.length; cardNumber++) {
+		cards[cardNumber] = cards[cardNumber].split(": ")[1];
+
+		let [winningNumbers, myNumbers] = cards[cardNumber].split("|");
+
+		winningNumbers = winningNumbers.trim();
+		winningNumbers = new Set(winningNumbers.split(" "));
+		myNumbers = myNumbers.trim().split(/\s+/);
+
+		scratchcards.set(cardNumber + 1, [winningNumbers, myNumbers]);
+	}
+
+	const scratchcardsIOwn = new Array([...scratchcards.keys()].length).fill(1);
+
+	for (let i = 0; i < scratchcardsIOwn.length; i++) {
+		let numberOfMatches = 0;
+
+		for (const number of scratchcards.get(i + 1)[1]) {
+			if (scratchcards.get(i + 1)[0].has(number)) {
+				numberOfMatches++;
+			}
+		}
+
+		for(let j = 1; j <= numberOfMatches; j++){
+            scratchcardsIOwn[i + j] += scratchcardsIOwn[i];
+        }
+	}
+
+	console.log(scratchcardsIOwn.reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+};
+
+partTwo(lines);

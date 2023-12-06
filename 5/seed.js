6c7963da2f78846feeed8caebc86fdb3b4a143c3
Bryan <morganthemosaic@gmail.com>
2023-12-05 19:23:05 -0500

    Indentation

--- 5/seed.js
+++ 5/seed.js
@@ -5,59 +5,59 @@ const filePath = "./inputs/puzzleInput.txt";
 const fileContent = fs.readFileSync(filePath, "utf-8");
 const groups = fileContent.split("\n\n");
 
-const partOne = groups => {
-    const seedsAndMaps = [];
+const partOne = (groups) => {
+	const seedsAndMaps = [];
 
-for (let group of groups) {
-	group = group.split(":")[1].trim().split("\n");
+	for (let group of groups) {
+		group = group.split(":")[1].trim().split("\n");
 
-	seedsAndMaps.push(group);
-}
-
-const seeds = seedsAndMaps[0][0].split(/\s+/);
-const maps = seedsAndMaps.slice(1);
-
-for (const map of maps) {
-	for (let i = 0; i < map.length; i++) {
-		map[i] = map[i].split(/\s+/);
-
-		for (let j = 0; j < map[i].length; j++) {
-			map[i][j] = parseInt(map[i][j]);
-		}
+		seedsAndMaps.push(group);
 	}
-}
 
-let lowestLocationValue = Infinity;
-
-for (const seed of seeds) {
-	let value = seed;
+	const seeds = seedsAndMaps[0][0].split(/\s+/);
+	const maps = seedsAndMaps.slice(1);
 
 	for (const map of maps) {
-		let potentialValue = value;
+		for (let i = 0; i < map.length; i++) {
+			map[i] = map[i].split(/\s+/);
 
-		for (const [
-			destinationRangeStart,
-			sourceRangeStart,
-			rangeLength,
-		] of map) {
-			if (value >= sourceRangeStart) {
-				const difference = value - sourceRangeStart;
-
-				if (difference < rangeLength) {
-					potentialValue = destinationRangeStart + difference;
-					break;
-				}
+			for (let j = 0; j < map[i].length; j++) {
+				map[i][j] = parseInt(map[i][j]);
 			}
 		}
-
-		value = potentialValue;
 	}
-    console.log(value);
 
-    lowestLocationValue = Math.min(value, lowestLocationValue);
-}
+	let lowestLocationValue = Infinity;
 
-console.log("\n" + lowestLocationValue);
-}
+	for (const seed of seeds) {
+		let value = seed;
+
+		for (const map of maps) {
+			let potentialValue = value;
+
+			for (const [
+				destinationRangeStart,
+				sourceRangeStart,
+				rangeLength,
+			] of map) {
+				if (value >= sourceRangeStart) {
+					const difference = value - sourceRangeStart;
+
+					if (difference < rangeLength) {
+						potentialValue = destinationRangeStart + difference;
+						break;
+					}
+				}
+			}
+
+			value = potentialValue;
+		}
+		console.log(value);
+
+		lowestLocationValue = Math.min(value, lowestLocationValue);
+	}
+
+	console.log("\n" + lowestLocationValue);
+};
 
 partOne(groups);

cffec13e062dd794305fbcd85a14fdd6943197fa
Bryan May <bryanmay@Bryans-MacBook-Air.local>
2023-12-14 06:46:46 -0800

    Part 2

--- 14/parabolic.js
+++ 14/parabolic.js
@@ -11,7 +11,7 @@ for (const row of platform) {
     platformAsArray.push(row.split(""));
 }
 
-const partOne = platformAsArray => {
+const tiltNorth = platformAsArray => {
     for (let column = 0; column < platformAsArray[0].length; column++) {
         let emptySpaceIndex = null;
         let row = 0;
@@ -36,6 +36,93 @@ const partOne = platformAsArray => {
             }
         }
     }
+}
+
+const tiltWest = platformAsArray => {
+    for (let row = 0; row < platformAsArray.length; row++) {
+        let emptySpaceIndex = null;
+        let column = 0;
+
+        while (column < platformAsArray[row].length) {
+            if (platformAsArray[row][column] === "." && emptySpaceIndex === null) {
+                emptySpaceIndex = column;
+                column++;
+            }
+            else if (platformAsArray[row][column] === "O" && Number.isInteger(emptySpaceIndex)) {
+                [platformAsArray[row][column], platformAsArray[row][emptySpaceIndex]] = [platformAsArray[row][emptySpaceIndex], platformAsArray[row][column]];
+
+                column = emptySpaceIndex + 1;
+                emptySpaceIndex = null;
+            }
+            else if (platformAsArray[row][column] === "#") {
+                emptySpaceIndex = null;
+                column++;
+            }
+            else {
+                column++;
+            }
+        }
+
+    }
+}
+
+const tiltSouth = platformAsArray => {
+    for (let column = 0; column < platformAsArray[0].length; column++) {
+        let emptySpaceIndex = null;
+        let row = platformAsArray.length - 1;
+
+        while (row >= 0) {
+            if (platformAsArray[row][column] === "." && emptySpaceIndex === null) {
+                emptySpaceIndex = row;
+                row--;
+            }
+            else if (platformAsArray[row][column] === "O" && Number.isInteger(emptySpaceIndex)) {
+                [platformAsArray[row][column], platformAsArray[emptySpaceIndex][column]] = [platformAsArray[emptySpaceIndex][column], platformAsArray[row][column]];
+
+                row = emptySpaceIndex - 1;
+                emptySpaceIndex = null;
+            }
+            else if (platformAsArray[row][column] === "#") {
+                emptySpaceIndex = null;
+                row--;
+            }
+            else {
+                row--;
+            }
+        }
+    }
+}
+
+const tiltEast = platformAsArray => {
+    for (let row = 0; row < platformAsArray.length; row++) {
+        let emptySpaceIndex = null;
+        let column = platformAsArray[row].length;
+
+        while (column >= 0) {
+            if (platformAsArray[row][column] === "." && emptySpaceIndex === null) {
+                emptySpaceIndex = column;
+                column--;
+            }
+            else if (platformAsArray[row][column] === "O" && Number.isInteger(emptySpaceIndex)) {
+                [platformAsArray[row][column], platformAsArray[row][emptySpaceIndex]] = [platformAsArray[row][emptySpaceIndex], platformAsArray[row][column]];
+
+                column = emptySpaceIndex - 1;
+                emptySpaceIndex = null;
+            }
+            else if (platformAsArray[row][column] === "#") {
+                emptySpaceIndex = null;
+                column--;
+            }
+            else {
+                column--;
+            }
+        }
+
+    }
+}
+
+const partOne = platformAsArray => {
+    tiltNorth(platformAsArray);
 
     let sum = 0;
 
@@ -46,4 +133,47 @@ const partOne = platformAsArray => {
     return sum;
 }
 
-console.log(partOne(platformAsArray))
+const partTwo = platformAsArray => {
+    const map = new Map();
+
+    for (let i = 0; i < 1000; i++) {
+        tiltNorth(platformAsArray);
+        tiltWest(platformAsArray);
+        tiltSouth(platformAsArray);
+        tiltEast(platformAsArray);
+
+        let s = platformAsArray.reduce((string, row) => string + row.join(""), "");
+
+        let sum = 0;
+
+        for (let j = 0; j < platformAsArray.length; j++) {
+            sum += platformAsArray[j].filter(value => value === "O").length * (platformAsArray.length - j);
+        }
+
+        let index = i;
+
+        if (i >= 82) {
+            index = ((i - 82) % 77) + 82
+        }
+
+        if (!map.has(s)) {
+            map.set(s, [index]);
+        }
+        else {
+            const indices = map.get(s);
+            indices.push(index);
+            map.set(s, indices);
+        }
+
+        if (index === 152) {
+            let sum = 0;
+
+            for (let j = 0; j < platformAsArray.length; j++) {
+                sum += platformAsArray[j].filter(value => value === "O").length * (platformAsArray.length - j);
+            }
+            return sum;
+        }
+    }
+}
+
+console.log(partTwo(platformAsArray))

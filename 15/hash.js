29c8e772c16ebfb10bb1a03cea4e5667219d25aa
morganthemosaic@gmail.com <bryanmay@Bryans-MacBook-Air.local>
2023-12-14 23:47:18 -0800

    Part 2

--- 15/hash.js
+++ 15/hash.js
@@ -19,4 +19,73 @@ const hash = step => {
     return currentValue;
 }
 
-console.log(steps.reduce((sum, step) => sum + hash(step), 0));
+const partOne = steps => {
+    return steps.reduce((sum, step) => sum + hash(step), 0);
+}
+
+const partTwo = steps => {
+    const boxes = new Map();
+
+    for (const step of steps) {
+        const label = step.match(/^[a-z]+/)[0];
+        const operator = step.match(/\W/)[0];
+        const boxNumber = hash(label);
+        const lens = step.split(/\W/).map(value => {
+            if (Number.isInteger(parseInt(value))) {
+                return parseInt(value);
+            }
+            else {
+                return value;
+            }
+        });
+
+        if (operator === "-") {
+            let lenses = boxes.get(boxNumber);
+
+            if (lenses) {
+                const indexOfLens = lenses.findIndex(lens => lens[0] === label);
+
+                if (indexOfLens > -1) {
+                    lenses = lenses.slice(0, indexOfLens).concat(lenses.slice(indexOfLens + 1));
+                    
+                    if(lenses.length){
+                        boxes.set(boxNumber, lenses);
+                    }
+                    else{
+                        boxes.delete(boxNumber)
+                    }
+                }
+            }
+        }
+        else {
+            let lenses = boxes.get(boxNumber);
+
+            if (lenses) {
+                const indexOfLens = lenses.findIndex(lens => lens[0] === label);
+
+                if (indexOfLens > -1) {
+                    lenses = lenses.slice(0, indexOfLens).concat([lens]).concat(lenses.slice(indexOfLens + 1));
+                    boxes.set(boxNumber, lenses);
+                }
+                else {
+                    lenses.push(lens)
+                }
+            }
+            else {
+                boxes.set(boxNumber, [lens]);
+            }
+        }
+    }
+
+    let sum = 0;
+
+    for(const [boxNumber, lenses] of boxes){
+        for(let lens = 0; lens < lenses.length; lens++){
+            sum += (boxNumber + 1) * (lens + 1) * lenses[lens][1];
+        }
+    }
+
+    return sum;
+}
+
+console.log(partTwo(steps))
